7e8726585ba25ea57a984128662a4000c0ba550a
jean0313 <denggang80@gmail.com>
2024-01-05 18:45:22 +0800

    feat: comply with springboot v2.5

--- src/main/java/com/demo/kafka/controller/KafkaProducerController.java
+++ src/main/java/com/demo/kafka/controller/KafkaProducerController.java
@@ -1,7 +1,9 @@
 package com.demo.kafka.controller;
 
-import com.demo.kafka.framework.ValidatorConfig;
 import com.demo.kafka.framework.producer.ProducerClient;
+import com.demo.kafka.framework.producer.ProducerClient2;
+import com.demo.kafka.framework.ValidatorConfig;
+import com.demo.kafka.model.AvroPersonInfo;
 import com.demo.kafka.model.PersonInfo;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -23,10 +29,6 @@ public class KafkaProducerController implements ApplicationContextAware {
 
     @Autowired
     private ProducerClient producerClient;
-
-//    @Autowired
-//    private ProducerClient2 producerClient2;
-
 
     @Autowired
     private ValidatorConfig config;
@@ -49,23 +51,9 @@ public class KafkaProducerController implements ApplicationContextAware {
                 MessageBuilder.withPayload(new PersonInfo(name, age))
                         .setHeader(KafkaHeaders.TOPIC, "demo").build(), Duration.ofSeconds(10));
         System.out.println(ret.getProducerRecord());
-
-//        ValidatorConfig validatorConfig = applicationContext.getBean(ValidatorConfig.class);
-//        System.out.println("config:" + validatorConfig.getConfig());
-
     }
 
-//    @GetMapping("/avro/{name}/{age}")
-//    public void sendAJson(@PathVariable("name") String name, @PathVariable("age") int age) throws ExecutionException, InterruptedException {
-//        CompletableFuture<SendResult<String, AvroPersonInfo>> ret = producerClient2.sendAvroMessage(
-//                MessageBuilder.withPayload(AvroPersonInfo.newBuilder().setName(name).setAge(age).build())
-//                        .setHeader(KafkaHeaders.TOPIC, "avro-topic").build());
-//        System.out.println(ret.get());
-//    }
-
-
     ApplicationContext applicationContext;
-
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
